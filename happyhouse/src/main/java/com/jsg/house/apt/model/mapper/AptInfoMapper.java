985d1e38becfdefdefc5ab1e6c479fc80d07d862
성광현 <gch03944@gmail.com>
2022-11-21 00:48:07 +0900

    [Add] kakao Api

--- happyhouse/src/main/java/com/jsg/house/apt/model/mapper/AptInfoMapper.java
+++ happyhouse/src/main/java/com/jsg/house/apt/model/mapper/AptInfoMapper.java
@@ -1,8 +1,11 @@
 package com.jsg.house.apt.model.mapper;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 
 public interface AptInfoMapper {
 	List<Object> getAptList(String code) throws SQLException;
+
+	List<Object> getsidogugunAptList(HashMap<String, String> map) throws SQLException;
 }
--- happyhouse/src/main/java/com/jsg/house/apt/model/service/AptInfoServiceImpl.java
+++ happyhouse/src/main/java/com/jsg/house/apt/model/service/AptInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jsg.house.apt.model.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,18 @@ public class AptInfoServiceImpl implements AptInfoService {
 	public List<Object> getAptList(String code) throws Exception {
 		// TODO Auto-generated method stub
 		return mapper.getAptList(code);
+	}
+
+	@Override
+	public List<Object> getDongNameAptList(String dongName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object> getsidogugunAptList(HashMap<String, String> map) throws Exception {
+		// TODO Auto-generated method stub
+		return mapper.getsidogugunAptList(map);
 	}
 	
 }
