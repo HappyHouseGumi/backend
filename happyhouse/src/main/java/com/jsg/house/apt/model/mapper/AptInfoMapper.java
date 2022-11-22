65a4f062bc86386f4ba5c12cdd07139586758a6b
성광현 <gch03944@gmail.com>
2022-11-22 15:38:37 +0900

    [Add] Apt Interest

--- happyhouse/src/main/java/com/jsg/house/apt/model/mapper/AptInfoMapper.java
+++ happyhouse/src/main/java/com/jsg/house/apt/model/mapper/AptInfoMapper.java
@@ -10,4 +10,8 @@ public interface AptInfoMapper {
 	List<Object> getsidogugunAptList(HashMap<String, String> map) throws SQLException;
 
 	List<Object> getAptDealList(String code) throws SQLException;
+	
+	List<Object> getUserInterApts(int userId) throws SQLException;
+	int addUserInterApt(HashMap<String, Object> map) throws SQLException;
+	int deleteUserInterApt(HashMap<String, Object> map) throws SQLException;
 }
